e18900f848ec9ac082dd5d96a629ed9e50260015
eafifa08 <eafifa07@yandex.ru>
2021-08-19 16:06:11 +0500

    first commit

---
+++ steganography.py
@@ -0,0 +1,121 @@
+# Steganography app by meshkov s.
+# bmp to modified bmp
+from bitstring import BitArray
+
+
+def rec_information(data_ints, message_size_in_bytes):
+    """Запись в список int информации о размере сообщения в байтах"""
+    interval_bytes = int.to_bytes(message_size_in_bytes, length=4, byteorder='big')
+    interval_ints = [byte for byte in interval_bytes]
+    data_ints = data_ints[:6] + interval_ints + data_ints[10:]
+    return data_ints
+
+
+def from_char_to_bits(char):
+    encoded_bytes = str.encode(char, 'utf-8')
+    bits = BitArray(encoded_bytes)
+    bits_array_ints = [int(bit) for bit in bits]
+    return bits_array_ints
+
+
+def from_bits_to_char(bits):
+    number = 0
+    for index, bit in enumerate(bits):
+        number += 2**(7-index)*bit
+    bytes_encoded = int.to_bytes(number, length=1, byteorder='big')
+    char = bytes.decode(bytes_encoded, 'utf-8')
+    return char
+
+
+def from_bits_to_int(bits):
+    number = 0
+    for index, bit in enumerate(bits):
+        number += 2**(7-index)*bit
+    return number
+
+
+def get_parametres_of_bmp_file(path_file):
+    with open(path_file, 'rb') as file:
+        file.seek(2)
+        bytes_size_of_file = file.read(2)
+        size_of_file = int.from_bytes(bytes_size_of_file, byteorder='little')
+        file.seek(10)
+        bytes_offset_of_image = file.read(4)
+        offset_of_image = int.from_bytes(bytes_offset_of_image, byteorder='little')
+        size_of_image = size_of_file - offset_of_image
+        file.seek(6)
+        size_of_message = int.from_bytes(file.read(4), byteorder='big')
+        result = {'size_of_file': size_of_file, 'offset_of_image': offset_of_image,
+                  'size_of_image': size_of_image, 'size_of_message': size_of_message}
+        return result
+
+
+def calculate_interval(size_of_image, message=0, size_of_message=0):
+    """Расчет интервала между байтами, содержащими изображение"""
+    if message:
+        message_bytes = message.encode('utf-8')
+        interval = int(int(size_of_image/len(message_bytes))/8)
+        if interval >= 1:
+            return interval
+        else: return -1
+    elif size_of_message:
+        interval = int(int(size_of_image/size_of_message)/8)
+        return interval
+    else:
+        return -1
+
+
+def write_message_to_bmp(path_file, message):
+    """Запись сообщения в bmp-файл"""
+    bmp_parametres = get_parametres_of_bmp_file(path_file)
+    interval = calculate_interval(message=message, size_of_image=bmp_parametres.get('size_of_image'))
+    if interval == -1:
+        return -1
+    with open(path_file, 'r+b') as file:
+        data_bytes = bytes(file.read())
+        data_ints = [one_byte for one_byte in data_bytes]
+        data_ints = rec_information(data_ints,  len(message.encode('utf-8')))
+        index = bmp_parametres.get('offset_of_image')
+        for char in message:
+            char_bits = from_char_to_bits(char)
+            for i in range(len(char_bits)):
+                byte = int.to_bytes(data_ints[index], length=1, byteorder='big')
+                bits = BitArray(byte)
+                bits_array_int = [int(bit) for bit in bits]
+                bits_array_int[7] = char_bits[i]
+                data_ints[index] = from_bits_to_int(bits_array_int)
+                index += interval
+        data_bytes_modified = bytes(data_ints)
+        file.seek(0)
+        file.write(data_bytes_modified)
+
+
+def read_message_from_bmp(path_file):
+    """Чтение сообщения из bmp-файла"""
+    bmp_parametres = get_parametres_of_bmp_file(path_file)
+    interval = calculate_interval(size_of_message=bmp_parametres.get('size_of_message'),
+                                  size_of_image=bmp_parametres.get('size_of_image'))
+    with open(path_file, 'rb') as file:
+        data_bytes = bytes(file.read())
+        data_ints = [one_byte for one_byte in data_bytes]
+        index = bmp_parametres.get('offset_of_image')
+        message_bits = []
+        message_ints = []
+        for i in range(bmp_parametres.get('size_of_message') * 8):
+            byte = int.to_bytes(data_ints[index], length=1, byteorder='big')
+            bits = BitArray(byte)
+            bits_array_int = [int(bit) for bit in bits]
+            message_bits.append(bits_array_int[7])
+            index += interval
+        for ind, val in enumerate(message_bits):
+            if (ind+1) % 8 == 0:
+                message_ints.append(from_bits_to_int(message_bits[ind-7:ind+1]))
+        message_bytes = bytes(message_ints)
+        message = bytes.decode(message_bytes, 'utf-8')
+        return message
+
+
+if __name__ == '__main__':
+    path = 'file_100x100x24.bmp'
+    write_message_to_bmp(path, 'привет, neo')
+    print(read_message_from_bmp(path))
