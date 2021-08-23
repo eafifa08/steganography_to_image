300c71307c179d37aefc9b6fadc2ffc8b1a51b45
eafifa08 <eafifa07@yandex.ru>
2021-08-23 21:59:02 +0500

    publish commit

--- steganography.py
+++ steganography.py
@@ -1,10 +1,23 @@
-# Steganography app by meshkov s.
-# bmp to modified bmp
+# Steganography app by meshkov sergey
+# bmp(png,jpg) to modified bmp
+# use function write_message_to_bmp(path_file, message) to put message in bmp file
+# example, write_message_to_bmp('example.bmp', 'wonderful message for you'):
+# use function read_message_from_bmp(path_file) to get message from bmp file
+# example, read_message_from_bmp('example.bmp') -> output:'wonderful message for you'
 from bitstring import BitArray
+from PIL import Image
+
+
+def convert_to_bmp(source_file):
+    """Convert png or jpeg to bmp"""
+    file = Image.open(source_file)
+    new_file = source_file[:-4] + '_with_message.bmp'
+    file.save(new_file)
+    return new_file
 
 
 def rec_information(data_ints, message_size_in_bytes):
-    """Запись в список int информации о размере сообщения в байтах"""
+    """Запись в список int информации о размере текстового сообщения в байтах"""
     interval_bytes = int.to_bytes(message_size_in_bytes, length=4, byteorder='big')
     interval_ints = [byte for byte in interval_bytes]
     data_ints = data_ints[:6] + interval_ints + data_ints[10:]
@@ -57,7 +70,8 @@ def calculate_interval(size_of_image, message=0, size_of_message=0):
         interval = int(int(size_of_image/len(message_bytes))/8)
         if interval >= 1:
             return interval
-        else: return -1
+        else:
+            return -1
     elif size_of_message:
         interval = int(int(size_of_image/size_of_message)/8)
         return interval
@@ -67,10 +81,11 @@ def calculate_interval(size_of_image, message=0, size_of_message=0):
 
 def write_message_to_bmp(path_file, message):
     """Запись сообщения в bmp-файл"""
+    path_file = convert_to_bmp(path_file)
     bmp_parametres = get_parametres_of_bmp_file(path_file)
     interval = calculate_interval(message=message, size_of_image=bmp_parametres.get('size_of_image'))
     if interval == -1:
-        return -1
+        return False
     with open(path_file, 'r+b') as file:
         data_bytes = bytes(file.read())
         data_ints = [one_byte for one_byte in data_bytes]
@@ -88,6 +103,7 @@ def write_message_to_bmp(path_file, message):
         data_bytes_modified = bytes(data_ints)
         file.seek(0)
         file.write(data_bytes_modified)
+    return True
 
 
 def read_message_from_bmp(path_file):
@@ -117,8 +133,6 @@ def read_message_from_bmp(path_file):
 
 if __name__ == '__main__':
     path = 'file_100x100x24.bmp'
-    path2 = 'image_bmp.bmp'
-    write_message_to_bmp(path, 'привет, neo')
-    write_message_to_bmp(path2,'вот так вот')
-    print(read_message_from_bmp(path))
-    print(read_message_from_bmp(path2))
+    path2 = 'file_100x100x24_with_message.bmp'
+    if write_message_to_bmp(path, 'привет, neo'):
+        print(read_message_from_bmp(path2))
